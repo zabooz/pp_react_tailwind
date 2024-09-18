c5996783eba444806bcabbd6ff745d52924b823b
zaboo <zabooz1988@gmail.com>
2024-09-18 23:47:46 +0200

    added links to button

--- src/content/landingPage/CardLanding.tsx
+++ src/content/landingPage/CardLanding.tsx
@@ -30,23 +30,23 @@ function CardCom({ data }: Props) {
       <p className="font-normal text-gray-700 dark:text-gray-400 ">
         {data.text}
       </p>
-      <Button className="mt-12 text-lg text-gray-200 tracking-wide ">
-      <Link to={data.btnLink}>
+      <Link to={data.btnLink} className="w-full">
+      <Button className="mt-12 text-lg text-gray-200 tracking-wide w-full ">
         {data.btnText}
-        </Link>
         <svg
           className="-mr-1 ml-2 h-5 w-4 items-center justify-center flex "
           fill="currentColor"
           viewBox="0 0 20 20"
           xmlns="http://www.w3.org/2000/svg"
-        >
+          >
           <path
             fillRule="evenodd"
             d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
             clipRule="evenodd"
-          />
+            />
         </svg>
       </Button>
+      </Link>
     </Card>
   );
 }

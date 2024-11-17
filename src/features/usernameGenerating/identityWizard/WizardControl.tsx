d70650c3277e4b1bcdef87ddcb86b0489246de64
zaboo <zabooz1988@gmail.com>
2024-11-17 12:58:48 +0100

    fixed wizard

--- src/features/usernameGenerating/identityWizard/WizardControl.tsx
+++ src/features/usernameGenerating/identityWizard/WizardControl.tsx
@@ -1,5 +1,5 @@
 import { Button } from 'flowbite-react';
-import { useMemo, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { FormattedMessage, useIntl } from 'react-intl';
 import { englishArraysObjAdjectives, englishArraysObjNouns } from '../../../data/translations/englishGenerator.data';
 import { userGenerator } from '../usernameGeneratingScript';
@@ -8,6 +8,10 @@ import WizardOutput from './WizardOutput';
 
 function WizardControl() {
     const [username, setUsername] = useState<HTMLSpanElement | null>(null);
+    const [traitOne, setTraitOne] = useState<string>('ages');
+    const [traitTwo, setTraitTwo] = useState<string>('ages');
+    const [noune, setNoune] = useState<string>('animals');
+    const [choices, setChoices] = useState<string[]>([traitOne, traitTwo, noune]);
     const intl = useIntl();
     const adjectives: string[] = useMemo(() => {
         return Array.from(Object.keys(englishArraysObjAdjectives)).sort((a, b) => a.localeCompare(b));
@@ -17,22 +21,31 @@ function WizardControl() {
     }, []);
 
     const handleClick = () => {
-        const choices: string[] = [];
-        document.querySelectorAll('.wizard-input select').forEach((item) => choices.push(item.value));
         setUsername(userGenerator({ choices }));
     };
+
+    useEffect(() => {
+        setChoices([traitOne, traitTwo, noune]);
+    }, [traitOne, traitTwo, noune]);
+
 
     return (
         <div>
             <WizardInput
                 wordList={adjectives}
                 value={intl.formatMessage({ id: 'adjective', defaultMessage: 'Eigenschaft: ' })}
+                onChange={setTraitOne}
             />
             <WizardInput
                 wordList={adjectives}
                 value={intl.formatMessage({ id: 'adjective2', defaultMessage: 'Eigenschaft: ' })}
+                onChange={setTraitTwo}
             />
-            <WizardInput wordList={nouns} value={intl.formatMessage({ id: 'noun', defaultMessage: 'Begriff: ' })} />
+            <WizardInput
+                wordList={nouns}
+                value={intl.formatMessage({ id: 'noun', defaultMessage: 'Begriff: ' })}
+                onChange={setNoune}
+            />
             <div className="grid grid-cols-2 gap-4 mt-10">
                 <Button className=" w-2/3 bg-[#ea6954]" onClick={() => handleClick()}>
                     <FormattedMessage id="generate-identity-wizard" defaultMessage="Los Geht's" />
--- src/features/usernameGenerating/identityWizard/WizardInput.tsx
+++ src/features/usernameGenerating/identityWizard/WizardInput.tsx
@@ -6,13 +6,16 @@ import { keyNames } from '@/data/translations/keysMessages';
 interface Props {
     wordList: string[];
     value: string;
+    onChange: (value: string) => void;
 }
 
-function WizardInput({ wordList, value }: Props) {
+function WizardInput({ wordList, value, onChange }: Props) {
     return (
         <div className="grid grid-cols-2 gap-4 my-2">
             <Label className="dark:text-gray-400 tracking-wider">{value}</Label>
-            <Select className="wizard-input">
+            <Select className="wizard-input"
+            onChange={(event) => onChange(event.target.value)}
+            >
                 {wordList.map((key, index) => (
                     <option key={key} value={wordList[index]}>
                         <FormattedMessage {...keyNames[key as keyof typeof keyNames]}/>

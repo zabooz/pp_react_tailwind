d64458ab9108ee1244b77293e118fb6983c462eb
zaboo <zabooz1988@gmail.com>
2024-11-17 12:33:10 +0100

    sdf

---
+++ src/data/translations/messages.ts
@@ -0,0 +1,120 @@
+import { defineMessages } from 'react-intl';
+
+export const keyNames = defineMessages({
+    colors: {
+        defaultMessage: 'Farben',
+        description: 'Colors_value',
+    },
+    shapes: {
+        defaultMessage: 'Formen',
+        description: 'Shapes_value',
+    },
+    textures: {
+        defaultMessage: 'Texturen',
+        description: 'Textures_value',
+    },
+    sizes: {
+        defaultMessage: 'Größen',
+        description: 'Sizes_value',
+    },
+    emotions: {
+        defaultMessage: 'Emotionen',
+        description: 'Emotions_value',
+    },
+    tastes: {
+        defaultMessage: 'Geschmäcker',
+        description: 'Tastes_value',
+    },
+    sounds: {
+        defaultMessage: 'Geräusche',
+        description: 'Sounds_value',
+    },
+    ages: {
+        defaultMessage: 'Alter',
+        description: 'Ages_value',
+    },
+    intensities: {
+        defaultMessage: 'Intensitäten',
+        description: 'Intensities_value',
+    },
+    temperatures: {
+        defaultMessage: 'Temperaturen',
+        description: 'Temperatures_value',
+    },
+    speeds: {
+        defaultMessage: 'Geschwindigkeiten',
+        description: 'Speeds_value',
+    },
+    qualities: {
+        defaultMessage: 'Qualitäten',
+        description: 'Qualities_value',
+    },
+    weatherTypes: {
+        defaultMessage: 'Wettertypen',
+        description: 'WeatherTypes_value',
+    },
+    animals: {
+        defaultMessage: 'Tiere',
+        description: 'Animals_value',
+    },
+    fantasyCreatures: {
+        defaultMessage: 'Fantasiewesen',
+        description: 'FantasyCreatures_value',
+    },
+    instruments: {
+        defaultMessage: 'Instrumente',
+        description: 'Instruments_value',
+    },
+    fruits: {
+        defaultMessage: 'Früchte',
+        description: 'Fruits_value',
+    },
+    vegetables: {
+        defaultMessage: 'Gemüse',
+        description: 'Vegetables_value',
+    },
+    rpgClasses: {
+        defaultMessage: 'RPG-Klassen',
+        description: 'RPGClasses_value',
+    },
+    fantasyRaces: {
+        defaultMessage: 'Fantasierassen',
+        description: 'FantasyRaces_value',
+    },
+    occupations: {
+        defaultMessage: 'Berufe',
+        description: 'Occupations_value',
+    },
+    vehicles: {
+        defaultMessage: 'Fahrzeuge',
+        description: 'Vehicles_value',
+    },
+    food: {
+        defaultMessage: 'Lebensmittel',
+        description: 'Food_value',
+    },
+    tools: {
+        defaultMessage: 'Werkzeuge',
+        description: 'Tools_value',
+    },
+    beverages: {
+        defaultMessage: 'Getränke',
+        description: 'Beverages_value',
+    },
+    clothingItems: {
+        defaultMessage: 'Kleidungsstücke',
+        description: 'ClothingItems_value',
+    },
+    bodyParts: {
+        defaultMessage: 'Körperteile',
+        description: 'BodyParts_value',
+    },
+    flowers: {
+        defaultMessage: 'Blumen',
+        description: 'Flowers_value',
+    },
+    desserts: {
+        defaultMessage: 'Desserts',
+        description: 'Desserts_value',
+    },
+});
